b075822d8b19eb082429c9c72a29c05d7d0fc02b
nikhil <nikhil@gmail.com>
2024-06-23 07:05:36 +0530

    slight corrections

--- routes/testimonialRoutes.js
+++ routes/testimonialRoutes.js
@@ -3,13 +3,21 @@ import {
   createTestimonial,
   getAllTestimonials,
 } from "../controllers/testimonialController.js";
+import { isAdmin, requireSignIn } from "./../middlewares/authMiddleware.js";
+import formidable from "express-formidable";
 
 const router = express.Router();
 
 // Route to create a new testimonial
-router.post("/create", createTestimonial);
+router.post("/create", requireSignIn, isAdmin, formidable(), createTestimonial);
 
 // Route to get all testimonials
-router.get("/get-all", getAllTestimonials);
+router.get(
+  "/get-all",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  getAllTestimonials
+);
 
 export default router;
