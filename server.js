617e5eb6680232f4049c536c2bf3f7cac31412ed
absurd_contributor <138147563+absurd-programmer@users.noreply.github.com>
2024-06-23 06:55:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import testimonialRoutes from "./routes/testimonialRoutes.js";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from 'url';
@@ -38,6 +39,7 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
+app.use("/api/v1/testimonials", testimonialRoutes);
 
 // Fallback to React app
 app.get("*", (req, res) => {
