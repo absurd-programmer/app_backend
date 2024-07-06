472eb9e9ae1b4cd5c14c4a406e545a5bf159e9e6
absurd_contributor <138147563+absurd-programmer@users.noreply.github.com>
2024-07-06 16:27:03 +0530

    Update blogRoutes.js

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -2,14 +2,18 @@ import express from "express";
 import { isAdmin, requireSignIn } from "./../middlewares/authMiddleware.js";
 import {
   createBlogController,
+  deleteBlogController,
   getBlogsController,
+  updateBlogController,
 } from "./../controllers/blogController.js";
 
 const router = express.Router();
 
 // Create blog
 router.post("/create-blog", requireSignIn, isAdmin, createBlogController);
-router.get("/get-blogs", getBlogsController);
+router.get("/", getBlogsController);
+router.put("/:id", updateBlogController);
+router.delete("/:id", deleteBlogController);
 // Other routes for update, get, delete can be added similarly
 
 export default router;
