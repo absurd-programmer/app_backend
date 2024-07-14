a39412364fb90d49a32d63b95204bf5913e77f18
absurd_contributor <138147563+absurd-programmer@users.noreply.github.com>
2024-07-14 23:18:14 +0530

    Update productModel.js

--- models/productModel.js
+++ models/productModel.js
@@ -6,10 +6,6 @@ const productSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    slug: {
-      type: String,
-      required: true,
-    },
     description: {
       type: String,
       required: true,
@@ -18,13 +14,17 @@ const productSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
-    category: {
-      type: mongoose.ObjectId,
-      ref: "Category",
-      required: true,
+    prices: {
+      type: Map,
+      of: Number,
     },
     quantity: {
       type: Number,
+      required: true,
+    },
+    category: {
+      type: mongoose.ObjectId,
+      ref: "Category",
       required: true,
     },
     photo: {
